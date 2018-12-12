9a8c4c54e8321d360af11c21aa169520b6180d15
Demba Siby <dembasiby@gmail.com>
2018-12-12 09:31:07 +0000

    Finalisation de la première version sans le calcul des soldes.

--- CasaEcologieSysInfo/UC_StockMatieresPremieres.Designer.cs
+++ CasaEcologieSysInfo/UC_StockMatieresPremieres.Designer.cs
@@ -28,20 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.adgvJournalStockMatieresPremieres = new ADGV.AdvancedDataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.adgvJournalStockMatieresPremieres)).BeginInit();
             this.SuspendLayout();
+            // 
+            // adgvJournalStockMatieresPremieres
+            // 
+            this.adgvJournalStockMatieresPremieres.AllowUserToAddRows = false;
+            this.adgvJournalStockMatieresPremieres.AllowUserToDeleteRows = false;
+            this.adgvJournalStockMatieresPremieres.AllowUserToResizeColumns = false;
+            this.adgvJournalStockMatieresPremieres.AllowUserToResizeRows = false;
+            this.adgvJournalStockMatieresPremieres.AutoGenerateContextFilters = false;
+            this.adgvJournalStockMatieresPremieres.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.adgvJournalStockMatieresPremieres.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.adgvJournalStockMatieresPremieres.DateWithTime = false;
+            this.adgvJournalStockMatieresPremieres.Location = new System.Drawing.Point(12, 73);
+            this.adgvJournalStockMatieresPremieres.Name = "adgvJournalStockMatieresPremieres";
+            this.adgvJournalStockMatieresPremieres.ReadOnly = true;
+            this.adgvJournalStockMatieresPremieres.Size = new System.Drawing.Size(968, 559);
+            this.adgvJournalStockMatieresPremieres.TabIndex = 0;
+            this.adgvJournalStockMatieresPremieres.TimeFilter = false;
             // 
             // UC_StockMatieresPremieres
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.adgvJournalStockMatieresPremieres);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "UC_StockMatieresPremieres";
             this.Size = new System.Drawing.Size(983, 635);
+            this.Load += new System.EventHandler(this.UC_StockMatieresPremieres_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.adgvJournalStockMatieresPremieres)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private ADGV.AdvancedDataGridView adgvJournalStockMatieresPremieres;
     }
 }
--- CasaEcologieSysInfo/UC_StockMatieresPremieres.cs
+++ CasaEcologieSysInfo/UC_StockMatieresPremieres.cs
@@ -12,9 +12,85 @@ namespace CasaEcologieSysInfo
 {
     public partial class UC_StockMatieresPremieres : UserControl
     {
+        CasaDBEntities2 db = new CasaDBEntities2();
+
         public UC_StockMatieresPremieres()
         {
             InitializeComponent();
         }
+
+        public static int CalculerSoldeStocks(int id)
+        {
+            CasaDBEntities2 db = new CasaDBEntities2();
+
+            int init = db.ResStockMatieresPremieres
+                .Where(s => s.CodeMatierePremiere == id)
+                .Select(m => m.StockMatiere).SingleOrDefault();
+
+            int entrees = db.EveReceptionMatieresPremieres
+                .Where(e => e.CodeMatierePremiere == id)
+                .Sum(q => q.Quantite);
+
+            int sorties = db.EveUtilisationMatieresPremieres
+                .Where(e => e.CodeMatierePremiere == id)
+                .Sum(q => q.QuantiteMatierePremiere);
+
+            return init + entrees - sorties;
+        }
+
+        private void UC_StockMatieresPremieres_Load(object sender, EventArgs e)
+        {
+            var Entrees = (from smp in db.ResStockMatieresPremieres
+                      from rmp in db.EveReceptionMatieresPremieres                     
+                      where smp.CodeMatierePremiere == rmp.CodeMatierePremiere
+
+                      select new 
+                      {
+                          DateOperation = rmp.DateReception,
+                          Description = smp.NomMatiere,
+                          Entree = rmp.Quantite,
+                          Sortie = 0,
+                          Solde = 0
+                                 
+                      });
+
+            var Sorties = (from smp in db.ResStockMatieresPremieres
+                           from ump in db.EveUtilisationMatieresPremieres
+                           from ur in db.EveUtilisationRessources
+                           from p in db.EveProductions
+                           where ump.CodeMatierePremiere == smp.CodeMatierePremiere
+                           where p.CodeUtilisationRessources == ur.CodeUtilisationRessources
+                           where ump.CodeUtilisationRessource == p.CodeUtilisationRessources
+
+                           select new
+                           {
+                               DateOperation = p.Date,
+                               Description = smp.NomMatiere,
+                               Entree = 0,
+                               Sortie = ump.QuantiteMatierePremiere,
+                               Solde = 0
+                           });
+
+            
+            var result = Entrees.Union(Sorties)
+                .OrderByDescending(d => d.DateOperation)
+                .ToList();  
+            
+            adgvJournalStockMatieresPremieres.DataSource = result;
+
+            // 
+
+        }
+
+
     }
 }
+
+    public class StockMatierePremiereViewModel
+    {
+        public DateTime Date { get; set; }
+        public string NomMatiere { get; set; }
+        public decimal Entree { get; set; }
+        public decimal Sortie { get; set; }
+        public int Solde { get; set; }       
+    }
