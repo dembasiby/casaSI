a2868477e1c20fdd874a9424ed3cc82787ddd4dd
Demba Siby <dembasiby@gmail.com>
2018-12-27 11:52:47 +0000

    Création d'un contrôle utilisateur pour accueillir les données de dettes fournisseurs

---
+++ CasaEcologieSysInfo/Pages/UC_DettesFournisseurs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasaEcologieSysInfo.Pages
+{
+    public partial class UC_DettesFournisseurs : UserControl
+    {
+        public UC_DettesFournisseurs()
+        {
+            InitializeComponent();
+        }
+    }
+}
