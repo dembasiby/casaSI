127e6a018cb61be473f2b3d0e3a14af85e308e22
Demba Siby <dembasiby@gmail.com>
2018-12-28 10:58:35 +0000

    Correction erreur de calcul dans le solde des dettes fournisseurs

--- CasaEcologieSysInfo/Pages/UC_DettesFournisseurs.Designer.cs
+++ CasaEcologieSysInfo/Pages/UC_DettesFournisseurs.Designer.cs
@@ -28,10 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbxListeFournisseursDettes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotalDettesFournisseurs = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(369, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 25);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Dettes fournisseurs";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(234, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(735, 481);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // lbxListeFournisseursDettes
+            // 
+            this.lbxListeFournisseursDettes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxListeFournisseursDettes.FormattingEnabled = true;
+            this.lbxListeFournisseursDettes.ItemHeight = 21;
+            this.lbxListeFournisseursDettes.Location = new System.Drawing.Point(13, 122);
+            this.lbxListeFournisseursDettes.Name = "lbxListeFournisseursDettes";
+            this.lbxListeFournisseursDettes.Size = new System.Drawing.Size(215, 399);
+            this.lbxListeFournisseursDettes.TabIndex = 28;
+            this.lbxListeFournisseursDettes.SelectedValueChanged += new System.EventHandler(this.LbxListeFournisseursDettes_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 21);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Total des dettes fournisseurs";
+            // 
+            // txtTotalDettesFournisseurs
+            // 
+            this.txtTotalDettesFournisseurs.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtTotalDettesFournisseurs.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotalDettesFournisseurs.Enabled = false;
+            this.txtTotalDettesFournisseurs.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalDettesFournisseurs.ForeColor = System.Drawing.Color.Red;
+            this.txtTotalDettesFournisseurs.Location = new System.Drawing.Point(17, 86);
+            this.txtTotalDettesFournisseurs.Name = "txtTotalDettesFournisseurs";
+            this.txtTotalDettesFournisseurs.Size = new System.Drawing.Size(211, 20);
+            this.txtTotalDettesFournisseurs.TabIndex = 30;
+            this.txtTotalDettesFournisseurs.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(431, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 23);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Détails des dettes fournisseurs";
+            // 
+            // UC_DettesFournisseurs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTotalDettesFournisseurs);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxListeFournisseursDettes);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "UC_DettesFournisseurs";
+            this.Size = new System.Drawing.Size(983, 635);
+            this.Load += new System.EventHandler(this.UC_DettesFournisseurs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox lbxListeFournisseursDettes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalDettesFournisseurs;
+        private System.Windows.Forms.Label label2;
     }
 }
--- CasaEcologieSysInfo/Pages/UC_DettesFournisseurs.cs
+++ CasaEcologieSysInfo/Pages/UC_DettesFournisseurs.cs
@@ -12,9 +12,191 @@ namespace CasaEcologieSysInfo.Pages
 {
     public partial class UC_DettesFournisseurs : UserControl
     {
+        CasaDBEntities2 db = new CasaDBEntities2();
+
         public UC_DettesFournisseurs()
         {
             InitializeComponent();
+        }
+
+        private void UC_DettesFournisseurs_Load(object sender, EventArgs e)
+        {
+            ListerFournisseurDettesFournisseurs();
+            txtTotalDettesFournisseurs.Text = Conversion.CalculerTotalDettesFournisseurs().ToString("c0");
+        }
+
+
+
+        private void ListerFournisseurDettesFournisseurs()
+        {
+            var fournisseursMP = (from fmp in db.AgeFournisseursMatieresPremieres
+                                    select new
+                                    {
+                                        NomFournisseur = fmp.Nom,
+                                        DetteInitial = (decimal?)fmp.SoldeDette ?? 0m,
+                                        MontantAchat = (decimal?)fmp.EveReceptionMatieresPremieres.Sum(s => s.Montant) ?? 0m,
+                                        MontantPaye = (decimal?)fmp.EveDecaissements.Sum(m => m.Montant) ?? 0m,
+                                        Solde = ((decimal?)fmp.SoldeDette ?? 0m)
+                                        + ((decimal?)fmp.EveReceptionMatieresPremieres.Sum(s => s.Montant) ?? 0m)
+                                        - ((decimal?)fmp.EveDecaissements.Sum(m => m.Montant) ?? 0m)
+                                    }
+                                 ).ToList();
+
+            var fournisseursEI = (from af in db.AgeAutreFournisseurs
+                                  select new
+                                  {
+                                      NomFournisseur = af.NomAutreFournisseur,
+                                      DetteInitial = (decimal?)af.SoldeInitialDetteFournisseur ?? 0m,
+                                      MontantAchat = (decimal?)af.EveReceptionEquipementsInfrastructures.Select(d => d.Montant).Sum() ?? 0m,
+                                      MontantPaye = (decimal?)af.EveDecaissements.Select(d => d.Montant).Sum() ?? 0m,
+                                      Solde = (decimal?)af.SoldeInitialDetteFournisseur ?? 0m + (decimal?)af.EveReceptionEquipementsInfrastructures
+                                      .Select(d => d.Montant).Sum() ?? 0m
+                                      - (decimal?)af.EveDecaissements.Select(d => d.Montant).Sum() ?? 0m
+                                  }).ToList();
+
+            var fournisseurFS = (from f in db.AgeFournisseursServicesFournitures
+                                 select new
+                                 {
+                                     NomFournisseur = f.NomFournisseurServiceFourniture,
+                                     DetteInitial = (decimal?)f.SoldeDette ?? 0m,
+                                     MontantAchat = (decimal?)f.EveAcquisitionServicesFournitures.Select(d => d.Montant).Sum() ?? 0m,
+                                     MontantPaye = (decimal?)f.EveDecaissements.Select(d => d.Montant).Sum() ?? 0m,
+                                     Solde = (decimal?)f.SoldeDette ?? 0m + (decimal?)f.EveAcquisitionServicesFournitures.Select(d => d.Montant).Sum() ?? 0m
+                                     - (decimal?)f.EveDecaissements.Select(d => d.Montant).Sum() ?? 0m
+                                 });
+
+            var listFournisseurs = fournisseursMP
+                .Union(fournisseursEI)
+                .Union(fournisseurFS)
+                .OrderByDescending(s => s.Solde)
+                .Select(f => f.NomFournisseur)
+                .ToList();
+           lbxListeFournisseursDettes.DataSource = listFournisseurs;
+        }
+
+        private void MontrerDetailsDettesFournisseurs(string nomFournisseur)
+        {
+            var achatMatierePrem = (from fmp in db.AgeFournisseursMatieresPremieres
+                                    from rmp in db.EveReceptionMatieresPremieres
+                                    where rmp.CodeFournisseurMatierePremiere == fmp.CodeFournisseurMatierePremiere
+                                    where fmp.Nom == nomFournisseur
+                                    select new
+                                    {
+                                        Date = rmp.DateReception,
+                                        MontantAchat = (decimal?)rmp.Montant ?? 0m,
+                                        MontantPaye = 0m,
+                                        Solde = 0m
+                                    }
+                                 );
+            var decaissementMP = (from fmp in db.AgeFournisseursMatieresPremieres
+                                  from d in db.EveDecaissements
+                                  where d.CodeFournisseurMatierePremiere == fmp.CodeFournisseurMatierePremiere
+                                  where fmp.Nom == nomFournisseur
+                                  select new
+                                  {
+                                      Date = d.DateDecaissement,
+                                      MontantAchat = 0m,
+                                      MontantPaye = (decimal?)d.Montant ?? 0m,
+                                      Solde = 0m
+                                  }
+                                 );
+
+            var achatEquipementInfr = (from af in db.AgeAutreFournisseurs
+                                       where af.NomAutreFournisseur == nomFournisseur
+                                    select new
+                                    {
+                                        Date = af.EveReceptionEquipementsInfrastructures.Select(d => d.DateReception).FirstOrDefault(),
+                                        MontantAchat = (decimal?)af.EveReceptionEquipementsInfrastructures.Select(s => s.Montant).FirstOrDefault() ?? 0m,
+                                        MontantPaye = 0m,
+                                        Solde = 0m
+                                    }
+                                 );
+
+            var decaisementEquip = (from af in db.AgeAutreFournisseurs
+                                where af.NomAutreFournisseur == nomFournisseur
+                                select new
+                                {
+                                    Date = af.EveDecaissements.Select(d => d.DateDecaissement).FirstOrDefault(),
+                                    MontantAchat = 0m,
+                                    MontantPaye = (decimal?)af.EveDecaissements.Select(m => m.Montant).FirstOrDefault() ?? 0m,
+                                    Solde = 0m
+                                }
+                                 );
+
+            var achatServFournitures = (from fs in db.AgeFournisseursServicesFournitures
+                                        where fs.NomFournisseurServiceFourniture == nomFournisseur
+                                    select new
+                                    {
+                                        Date = fs.EveAcquisitionServicesFournitures.Select(d => d.Date).FirstOrDefault(),
+                                        MontantAchat = (decimal?)fs.EveAcquisitionServicesFournitures.Select(s => s.Montant).FirstOrDefault() ?? 0m,
+                                        MontantPaye = 0m,
+                                        Solde = 0m
+                                    }
+                                 );
+
+            var decaissementServFourn = (from fs in db.AgeFournisseursServicesFournitures
+                                        where fs.NomFournisseurServiceFourniture == nomFournisseur
+                                        select new
+                                        {
+                                            Date = fs.EveDecaissements.Select(d => d.DateDecaissement).FirstOrDefault(),
+                                            MontantAchat = 0m,
+                                            MontantPaye = (decimal?)fs.EveDecaissements.Select(m => m.Montant).FirstOrDefault() ?? 0m,
+                                            Solde = 0m
+                                        }
+                                 );
+            var totalOperations = achatMatierePrem.Union(decaissementMP)
+                .Union(achatEquipementInfr)
+                .Union(decaisementEquip)
+                .Union(achatServFournitures)
+                .Union(decaissementServFourn)
+                .ToList();
+
+            DataTable dt = Conversion.ConvertirEnTableDeDonnees(totalOperations);
+            DataRow dr = dt.NewRow();
+            dt.Rows.InsertAt(dr, 0);
+            dr["MontantAchat"] = 0;
+            dr["MontantPaye"] = 0;
+            dr["Solde"] = 0;
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["MontantAchat"].DefaultCellStyle.Format = "c0";
+            dataGridView1.Columns["MontantPaye"].DefaultCellStyle.Format = "c0";
+            dataGridView1.Columns["Solde"].DefaultCellStyle.Format = "c0";
+
+            var detteFournisseurInitialMP = (from fmp in db.AgeFournisseursMatieresPremieres
+                                             where fmp.Nom == nomFournisseur
+                                             select (decimal?)fmp.SoldeDette).Sum() ?? 0m;
+                     
+            var detteFournisseurInitialAF = (from af in db.AgeAutreFournisseurs
+                                             where af.NomAutreFournisseur == nomFournisseur
+                                             select (decimal?)af.SoldeInitialDetteFournisseur).Sum() ?? 0m;
+
+            var detteFournisseurInitialFS = (from fs in db.AgeFournisseursServicesFournitures
+                                             where fs.NomFournisseurServiceFourniture == nomFournisseur
+                                             select (decimal?)fs.SoldeDette).Sum() ?? 0m;
+
+            var soldeInitial = detteFournisseurInitialMP + detteFournisseurInitialAF + detteFournisseurInitialFS;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (i > 0)
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[3].Value)
+                    + Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value)
+                    - Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+                }
+                else
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = soldeInitial + Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value)
+                    - Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+                }
+            }
+        }
+
+        private void LbxListeFournisseursDettes_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var nomFournisseur = lbxListeFournisseursDettes.GetItemText(lbxListeFournisseursDettes.SelectedItem);
+            MontrerDetailsDettesFournisseurs(nomFournisseur); 
         }
     }
 }
