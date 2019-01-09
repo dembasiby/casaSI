b7f4657dc583dabaeffb2e076f12d092469123b4
Demba Siby <dembasiby@gmail.com>
2019-01-09 10:42:23 +0000

    Modification de la requête d'affichage du journal des stocks de produits semi-finis

--- CasaEcologieSysInfo/ResStockProduitsSemiFini.cs
+++ CasaEcologieSysInfo/ResStockProduitsSemiFini.cs
@@ -24,6 +24,7 @@ namespace CasaEcologieSysInfo
         public int CodeProduitSemiFini { get; set; }
         public int CodeMatierePremiere { get; set; }
         public int Quantite { get; set; }
+        public string Description { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<EveProductionProduitsSemiFini> EveProductionProduitsSemiFinis { get; set; }
