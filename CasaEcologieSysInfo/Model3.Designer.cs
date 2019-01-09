11a6c575e3c9933b68f370b29266fa4adb66bef9
Demba Siby <dembasiby@gmail.com>
2019-01-09 11:17:47 +0000

    Regénération du modèle à la suite du chamgement de type int à float des quantités de matière première

--- CasaEcologieSysInfo/Model3.Designer.cs
+++ CasaEcologieSysInfo/Model3.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\hp pro\source\repos\CasaEcologieSysInfo\CasaEcologieSysInfo\Model3.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\dsiby_000\source\repos\CasaEcologieSysInfo\CasaEcologieSysInfo\Model3.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
