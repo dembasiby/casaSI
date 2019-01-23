f4bc9bd10ed1fe180453f44a8af4462709a13606
Demba Siby <dembasiby@gmail.com>
2019-01-23 16:38:57 +0000

    Création du journal de la production

---
+++ CasaEcologieSysInfo/Pages/UC_JournalProduction.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace CasaEcologieSysInfo.Pages
+{
+    partial class UC_JournalProduction
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbxDatesProduction = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Journal de Production";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbxDatesProduction
+            // 
+            this.lbxDatesProduction.FormattingEnabled = true;
+            this.lbxDatesProduction.ItemHeight = 17;
+            this.lbxDatesProduction.Location = new System.Drawing.Point(557, 137);
+            this.lbxDatesProduction.Name = "lbxDatesProduction";
+            this.lbxDatesProduction.Size = new System.Drawing.Size(146, 225);
+            this.lbxDatesProduction.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(319, 506);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // UC_JournalProduction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbxDatesProduction);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "UC_JournalProduction";
+            this.Size = new System.Drawing.Size(983, 596);
+            this.Load += new System.EventHandler(this.UC_JournalProduction_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxDatesProduction;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ CasaEcologieSysInfo/Pages/UC_JournalProduction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasaEcologieSysInfo.Pages
+{
+    public partial class UC_JournalProduction : UserControl
+    {
+        CasaDBEntities2 db = new CasaDBEntities2();
+
+        public UC_JournalProduction()
+        {
+            InitializeComponent();
+        }
+
+        private void UC_JournalProduction_Load(object sender, EventArgs e)
+        {
+            AfficherDatesProduction();
+        }
+
+        private void AfficherDatesProduction()
+        {
+            var listeProduction = (from prod in db.EveProductions
+                                   select new
+                                   {
+                                       prod.Date,
+                                       Code = prod.CodeProduction
+                                   })
+                                   .ToList();
+
+            DataTable dt = Conversion.ConvertirEnTableDeDonnees(listeProduction);
+            //lbxDatesProduction.DataSource = dt;
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
