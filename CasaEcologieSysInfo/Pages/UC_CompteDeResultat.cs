d8ac7f9eb9773702176b49020343963ed84cf8aa
Demba Siby <dembasiby@gmail.com>
2019-01-25 16:42:33 +0000

    Remplacement des textbox par un datagridview pour afficher le compte de resultat

--- CasaEcologieSysInfo/Pages/UC_CompteDeResultat.cs
+++ CasaEcologieSysInfo/Pages/UC_CompteDeResultat.cs
@@ -18,35 +18,28 @@ namespace CasaEcologieSysInfo.Pages
         public UC_CompteDeResultat()
         {
             InitializeComponent();
+            AfficherAnnees();
+        }
+
+        private void AfficherAnnees()
+        {
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "yyyy";
             dateTimePicker1.ShowUpDown = true;
         }
 
-        private string CalculerVentesMois(int NumMois)
+        private string CalculerVentesMois(int NumMois, DateTime annee)
         {
             return ((from v in db.EveVentes
                     join vp in db.EveVenteStockProduitsFinis on v.CodeVente equals vp.CodeVente
-                    where v.DateVente.Year == DateTime.Now.Year
+                    where v.DateVente.Year == annee.Year
                     where v.DateVente.Month == NumMois
                     select (decimal?)vp.Montant).Sum() ?? 0m).ToString("n0");
         }
 
         private void AfficherVentesMensuelles()
         {
-            txtVentesJan.Text = CalculerVentesMois(1);
-            txtVentesFev.Text = CalculerVentesMois(2);
-            txtVentesMars.Text = CalculerVentesMois(3);
-            txtVentesAvr.Text = CalculerVentesMois(4);
-            txtVentesMai.Text = CalculerVentesMois(5);
-            txtVentesJun.Text = CalculerVentesMois(6);
-            txtVentesJul.Text = CalculerVentesMois(7);
-            txtVentesAou.Text = CalculerVentesMois(8);
-            txtVentesSep.Text = CalculerVentesMois(9);
-            txtVentesOct.Text = CalculerVentesMois(10);
-            txtVentesNov.Text = CalculerVentesMois(11);
-            txtVentesDec.Text = CalculerVentesMois(12);
-
+            
         }
 
 
@@ -56,7 +49,7 @@ namespace CasaEcologieSysInfo.Pages
             AfficherVentesMensuelles();
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
 
         }
