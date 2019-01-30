da54df5b132c9345597b0180014c52db6dab1ed7
Demba Siby <dembasiby@gmail.com>
2019-01-30 14:57:00 +0000

    Création d'une nouvelle classe Validation pour gérer la validation des données

---
+++ CasaEcologieSysInfo/Validation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CasaEcologieSysInfo
+{
+    class Validation
+    {
+        public static bool VerifierChampsMontant(string nomChamps)
+        {
+            if (string.IsNullOrEmpty(nomChamps))
+            {
+                MessageBox.Show("Ce champs doit être renseigné.");
+                return false;
+            }
+
+            try
+            {
+                int temp = Convert.ToInt32(nomChamps);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ce champs doit contenir uniquement des nombres.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
