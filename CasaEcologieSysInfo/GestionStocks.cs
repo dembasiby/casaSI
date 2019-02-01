f40ff38d8879db030d2835374f471bcab4c45b4e
Demba Siby <dembasiby@gmail.com>
2019-02-01 15:04:16 +0000

    Ajout d'une nouvelle classe pour la gestion des fonctions de gestion des stocks

---
+++ CasaEcologieSysInfo/GestionStocks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaEcologieSysInfo
+{
+    class GestionStocks
+    {
+        public static Single MatieresPremieresUtilisees(DateTime beginning, DateTime ending)
+        {
+            // Raw materials used = Beginning raw materials inventory + Purchase - Ending raw materials inventory
+
+            using (CasaDBEntities2 db = new CasaDBEntities2())
+            {
+                var prixUnitaire = (from amp in db.eve)
+            }
+        }
+    }
+}
