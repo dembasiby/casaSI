637e5122ff7057764b76666225f3e61e94a8ca48
Demba Siby <dembasiby@gmail.com>
2019-12-25 20:49:14 +0000

    Réorganisation des fonctions

--- CasaEcologieSysInfo/Pages/UC_EtatsFinanciers.cs
+++ CasaEcologieSysInfo/Pages/UC_EtatsFinanciers.cs
@@ -1,15 +1,11 @@
 ﻿using CasaEcologieSysInfo.Classes;
-using CasaEcologieSysInfo.Classes.CalculDesCouts;
 using System;
-using System.Data;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace CasaEcologieSysInfo.Pages
 {
     public partial class UC_EtatsFinanciers : UserControl
     {
-
         CasaDBEntities db = new CasaDBEntities();
 
         public UC_EtatsFinanciers()
@@ -17,11 +13,23 @@ namespace CasaEcologieSysInfo.Pages
             InitializeComponent();
         }
 
+        // GESTION DES EVENEMENTS
         private void UC_EtatsFinMensuels_Load(object sender, EventArgs e)
         {
             AfficherEtatsFinanciers();
         }
 
+        private void DtpDebut_ValueChanged(object sender, EventArgs e)
+        {
+            AfficherEtatsFinanciers();
+        }
+
+        private void DtpFin_ValueChanged(object sender, EventArgs e)
+        {
+            AfficherEtatsFinanciers();
+        }
+
+        // GESTION DE L'AFFICHAGE DES TABLEAUX
         private void AfficherEtatsFinanciers()
         {
             AfficherCompteDeResultat();
@@ -47,34 +55,25 @@ namespace CasaEcologieSysInfo.Pages
             Bilan.AfficherBilan(dgvBilan, dtpDebut, dtpFin);
         }
 
-
         private void ReInitialiserTableau(DataGridView tableau)
         {
             tableau.Rows.Clear();
         }
-
-    
-        // GESTION DES EVENEMENTS
-        private void DtpDebut_ValueChanged(object sender, EventArgs e)
-        {
-            AfficherEtatsFinanciers();
-        }
-
-        private void DtpFin_ValueChanged(object sender, EventArgs e)
-        {
-            AfficherEtatsFinanciers();
-        }
-
+     
         //  IMPRESSION DES TABLEAUX
         private void BtnImprimerCpteResultat_Click(object sender, EventArgs e)
         {
-            string titre = "Compte de résultats du " + dtpDebut.Value.ToShortDateString() + " au " + dtpFin.Value.ToShortDateString();
+            string titre = "Compte de résultats du " 
+                         + dtpDebut.Value.ToShortDateString() 
+                         + " au " + dtpFin.Value.ToShortDateString();
             Impression.ImprimerTableau(titre, dgvCpteResultats);
         }
 
         private void BtnImprimerTablTresorerie_Click(object sender, EventArgs e)
         {
-            string titre = "Tableau de trésorerie du " + dtpDebut.Value.ToShortDateString() + " au " + dtpFin.Value.ToShortDateString();
+            string titre = "Tableau de trésorerie du " 
+                         + dtpDebut.Value.ToShortDateString() 
+                         + " au " + dtpFin.Value.ToShortDateString();
             Impression.ImprimerTableau(titre, dgvTableauTresorerie);
         }
 
